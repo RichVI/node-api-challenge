0ba081a4ed5d79eecb3dd1668380d552be489536
RichVI <richardwang2050@gmail.com>
2020-03-13 11:04:55 -0700

    CORS added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const projectRouter = require('./routers/projectRouter');
 const actionRouter = require('./routers/actionRouter');
 
@@ -6,6 +7,7 @@ const actionRouter = require('./routers/actionRouter');
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use(logger);
 server.use('/api/projects', projectRouter);
 server.use('/api/actions', actionRouter);
