15630e585d4da308cc7230770ecd4172029a11a9
RichVI <richardwang2050@gmail.com>
2020-03-13 10:10:02 -0700

    Testing Heroku

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+require('dotenv').config();
+
 const server = require('./server');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => (console.log (`\n** server running on http://localhost:${PORT} **\n`)));
